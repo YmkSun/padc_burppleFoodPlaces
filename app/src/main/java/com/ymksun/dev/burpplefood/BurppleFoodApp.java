f379931e982bd2a6daa05eb64a77b85b1231f9cc
Ye Myo Kyaw <ymksun90@gmail.com>
2018-01-06 15:30:57 +0630

    Skeleton Structure...

---
+++ app/src/main/java/com/ymksun/dev/burpplefood/BurppleFoodApp.java
@@ -0,0 +1,17 @@
+package com.ymksun.dev.burpplefood;
+
+import android.app.Application;
+
+/**
+ * Created by yemyokyaw on 1/6/18.
+ */
+
+public class BurppleFoodApp extends Application {
+
+    public static final String LOG_TAG = "BurppleFoodApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/ymksun/dev/burpplefood/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ymksun.dev.burpplefood.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ymksun.dev.burpplefood.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
